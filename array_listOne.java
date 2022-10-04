61f4625fea875cd10b41652419d36bd75ee50d77
Merazul Islam <107124645+Merazul-Islam-jp@users.noreply.github.com>
2022-10-03 22:15:22 -0700

    Add files via upload

---
+++ array_listOne.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class array_listOne {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+        Scanner user = new Scanner(System.in);
+
+        for (int i = 0; i < 4; i++) {
+            String name = user.nextLine();
+
+            al.add(name);
+        }
+        Iterator iterator = (Iterator) al.iterator();
+
+        System.out.println("Iterator item:");
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+}
